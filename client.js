35cb4541c2ed2f9f0d3b9695d7c9800aa408c0bd
meriem farsi <farsi.meriem60@gmail.com>
2020-08-25 18:45:22 +0000

    setupInput

--- client.js
+++ client.js
@@ -13,7 +13,24 @@ const connect = function() {
   conn.on('data', (message)=>{console.log(`server send : ${message}`)})
   return conn;
 };
+const setupInput = function() {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding('utf8');
+  stdin.resume();
+  handleUserInput();
+ return stdin;
+}
+const handleUserInput = function() {
+  process.stdin.on('data', (key) => {
+   if (key === '\u0003') {
+     process.exit();
+   }
+ });
+ };
 let object = {
   connect,
+  setupInput,
+  handleUserInput,
 };
 module.exports = object;
