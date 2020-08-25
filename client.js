02c3199f5a870d553c2ed6a9e72e4d99b3176942
meriem farsi <farsi.meriem60@gmail.com>
2020-08-25 19:26:13 +0000

    use constants

--- client.js
+++ client.js
@@ -1,8 +1,12 @@
 const net = require('net');
+const {IP, PORT} = require('./constants');
 const connect = function() {
   const conn = net.createConnection({ 
-    host: '127.0.0.1',
-    port: 50541
+    //host: '127.0.0.1',
+    //port: 50541
+    host: IP,
+    port: PORT,
+
   });
   conn.setEncoding('utf8'); 
   conn.on('connect', () => {
---
+++ constants.js
@@ -0,0 +1,7 @@
+const IP = '127.0.0.1';
+const PORT = 50541; 
+module.exports = {
+ IP,
+ PORT  
+};
+
